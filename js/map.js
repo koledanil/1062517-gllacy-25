3545ea8552507d4e505697bd08ec1b4b3c5e86e4
Danylo <koledanil@gmail.com>
2019-06-23 14:52:17 +0300

    Устранение багов + улучшения

---
+++ js/map.js
@@ -0,0 +1,36 @@
+
+
+// function initMap() {
+//        var map = new google.maps.Map(document.getElementById('map'), {
+//          zoom: 17,
+//          center: {lat: 59.938867, lng: 30.322962}
+//        });
+//
+//        var image = '/Users/irw/Documents/GitHub/1062517-gllacy-25/img/icon-pin-map.png';
+//        var beachMarker = new google.maps.Marker({
+//          position: {lat: 59.938867, lng: 30.322962},
+//          map: map,
+//          icon: image
+//        });
+//      }
+
+
+
+
+
+
+
+
+
+// function initMap() {
+//   // The location of Uluru
+//   var uluru = {lat: 59.938867, lng: 30.322962};
+//   // The map, centered at Uluru
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 17, center: uluru});
+//
+//   var iconBase =
+//           'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+//   // The marker, positioned at Uluru
+//   var marker = new google.maps.Marker({position: uluru, map: map});
+// }
